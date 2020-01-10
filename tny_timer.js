c219a24597cd6f9ea929e76065b31227458428e9
Dominic <dtorres243@west-mec.org>
2020-01-10 16:35:43 -0700

    fixed and finished the review

--- tny_timer.js
+++ tny_timer.js
@@ -11,17 +11,31 @@
 
 */
 
+showClock();
+setInterval("showClock()", 1000);
+
 //step 5
 function showClock() {
-   var thisDay = new Date("May 19, 2018 09:31:27");
-   var localDate = thisDay.toLocalDateString();
-   var localTime = thisDay.toLocalTimeString();
+   var thisDay = new Date();
+   var localDate = thisDay.toLocaleDateString();
+   var localTime = thisDay.toLocaleTimeString();
    document.getElementById("currentTime").innerHTML="<span>" + localDate + "</span><span>" + localTime + "</span>";
-   var j4Date = 
+   var j4Date = nextJuly4(thisDay);
+   j4Date.setHours(21, 0, 0, 0);
+
+   var days = (j4Date - thisDay)/(1000*60*60*24);
+   var hrs = (days - Math.floor(days))*24;
+   var mins = (hrs - Math.floor(hrs))*60;
+   var secs = (mins - Math.floor(mins))*60;
+
+   document.getElementById("dLeft").innerHTML = Math.floor(days);
+   document.getElementById("hLeft").innerHTML = Math.floor(hrs);
+   document.getElementById("mLeft").innerHTML = Math.floor(mins);
+   document.getElementById("sLeft").innerHTML = Math.floor(secs);
 }
 
-function nextJuly4(thisDay) {
-   var cYear = thisDay.getFullYear();
+function nextJuly4(currentDate) {
+   var cYear = currentDate.getFullYear();
    var jDate = new Date("July 4, 2018");
    jDate.setFullYear(cYear);
    if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
